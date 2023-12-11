ae8c79622ca428032df004a0c2f4f541b54dc478
Pranesh-12345 <125364788+Pranesh-12345@users.noreply.github.com>
2023-12-11 13:27:11 +0530

    Update Serial.py

--- Serial.py
+++ Serial.py
@@ -2,7 +2,22 @@
 
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace '/dev/ttyUSB0' with the actual port
+# Automatically detect the Arduino port
+def detect_arduino_port():
+    ports = [f'/dev/ttyUSB{i}' for i in range(10)]  # Assuming the Arduino is connected to one of these ports
+
+    for port in ports:
+        try:
+            ser = serial.Serial(port, 9600, timeout=1)
+            ser.close()
+            return port
+        except serial.SerialException:
+            pass
+
+    raise Exception("Arduino port not found")
+
+arduino_port = detect_arduino_port()
+ser = serial.Serial(arduino_port, 9600, timeout=1)
 
 while True:
     if ser.in_waiting > 0:
