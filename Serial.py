cb7a249728937933729cde6bc9ccbd6ca0ca411d
Pranesh-12345 <125364788+Pranesh-12345@users.noreply.github.com>
2023-12-11 13:38:18 +0530

    Update Serial.py

--- Serial.py
+++ Serial.py
@@ -1,25 +1,34 @@
-# Raspberry Pi Code for Slave
 
 import serial
+import time
 
-# Automatically detect the Arduino port
-def detect_arduino_port():
-    ports = [f'/dev/ttyUSB{i}' for i in range(10)]  # Assuming the Arduino is connected to one of these ports
-
-    for port in ports:
-        try:
-            ser = serial.Serial(port, 9600, timeout=1)
-            ser.close()
-            return port
-        except serial.SerialException:
-            pass
-
-    raise Exception("Arduino port not found")
-
-arduino_port = detect_arduino_port()
-ser = serial.Serial(arduino_port, 9600, timeout=1)
+# Create a serial object (change '/dev/ttyUSB0' to your Arduino's serial port)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
 while True:
-    if ser.in_waiting > 0:
-        message = ser.readline().decode('utf-8').rstrip()
-        print(f'Message from Arduino: {message}')
+    # Read data from Arduino
+    data = ser.readline().decode().strip()  # Decode bytes to string and strip newline characters
+    
+    if data:
+        print("Data received from Arduino:", data)
+        
+        # Send acknowledgment or response back to Arduino
+        ser.write("Received: ".encode() + data.encode())
+        
+    time.sleep(0.5)  # Adjust delay as needed
+
+
+Arduino code for Master (communicating with the Raspberry Pi as slave):
+
+arduino
+void setup() {
+  Serial.begin(9600);  // Initialize serial communication
+}
+
+void loop() {
+  // Send data to Raspberry Pi
+  Serial.println("Hello Raspberry Pi!");
+  delay(1000); // Adjust delay as needed
+}
+
+
