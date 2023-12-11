56813a2832068f1177e3001d1b25063fb898f20f
Pranesh-12345 <125364788+Pranesh-12345@users.noreply.github.com>
2023-12-11 13:19:06 +0530

    Update Serial.py

--- Serial.py
+++ Serial.py
@@ -1,18 +1,10 @@
+# Raspberry Pi Code for Slave
+
 import serial
-import time
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace '/dev/ttyUSB0' with the actual port your Arduino is connected to
+ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace '/dev/ttyUSB0' with the actual port
 
-try:
-    while True:
-        # Send a request from Raspberry Pi to Arduino
-        ser.write(b'R')  # Sending the letter 'R' to request RPM data
-
-        # Wait for the response
-        if ser.in_waiting > 0:
-            data = ser.readline().decode('utf-8').rstrip()
-            print(data)
-        time.sleep(1)  # Adjust this delay based on your Arduino loop timing
-except KeyboardInterrupt:
-    ser.close()
-    print("Serial connection closed.")
+while True:
+    if ser.in_waiting > 0:
+        message = ser.readline().decode('utf-8').rstrip()
+        print(f'Message from Arduino: {message}')
